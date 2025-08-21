c606c867b91413f420bff8d509a790f8c5368b58
Ansh Rawat <anshhh713@gmail.com>
2025-08-21 13:46:19 +0530

    4

---
+++ src/navbar.tsx
@@ -0,0 +1,13 @@
+import './navbar.css'
+function Navbar(){
+    return (
+        <>
+            <div className="navbar">
+                <img src="" alt="logo" />
+                <input type="search" placeholder="Search..." />
+                <img src="" alt="profile" />
+            </div>
+        </>
+    )
+}
+export default Navbar
